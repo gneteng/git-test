0daa998f3d57cc52f17340573ac513cba22bd37a
gnet <gnet009@163.com>
2020-07-15 16:44:10 +0800

    修改firstClass

--- git-test-project/src/com/itheima/git/FirstClass.java
+++ git-test-project/src/com/itheima/git/FirstClass.java
@@ -4,6 +4,9 @@ public class FirstClass {
 
     public static void main(String[] args) {
         System.out.println("hello git");
+
+        //test
+        System.out.println("hello world");
     }
 
     public void test(){
